614142e3f196498bd45d5e993985a2db67df7b6a
w4.3nd0 <w4.3nd0@gmail.com>
2026-02-11 16:17:00 +0900

    chore: vitest.config.ts修正(#21) - Backend CIのテスト実行時に環境変数の設定値がないことにより失敗していたので修正

--- backend/vitest.config.ts
+++ backend/vitest.config.ts
@@ -4,5 +4,12 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'node',
+    env: {
+      PORT: '3001',
+      HOST: '0.0.0.0',
+      NODE_ENV: 'test',
+      CORS_ORIGIN: 'http://localhost:5173',
+      LOG_LEVEL: 'error', // NOTE: test時はログ抑制
+    },
   },
 });
